3a9a92318e0da5ac98b2580a5801f7d614c9b2a3
mabster314 <mabster314@gmail.com>
2015-01-17 20:49:15 -0600

    Made a PID subsystem for the elevator as well as a command that will hopefully move it to a position.

--- RobotCode/src/org/usfirst/frc/team2175/robot/Robot.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/Robot.java
@@ -1,12 +1,14 @@
 
 package org.usfirst.frc.team2175.robot;
 
+import org.usfirst.frc.team2175.robot.commands.ExampleCommand;
+import org.usfirst.frc.team2175.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team2175.robot.subsystems.PIDToteElevator;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team2175.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2175.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,6 +20,7 @@ import org.usfirst.frc.team2175.robot.subsystems.ExampleSubsystem;
 public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final PIDToteElevator pidToteElevator = new PIDToteElevator();
 	public static OI oi;
 
     Command autonomousCommand;
--- RobotCode/src/org/usfirst/frc/team2175/robot/RobotMap.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/RobotMap.java
@@ -1,4 +1,10 @@
 package org.usfirst.frc.team2175.robot;
+
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -10,6 +16,11 @@ public class RobotMap {
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
+	
+	public static Talon toteElevatorTalon = new Talon(0);
+	public static DigitalInput topSwitch = new DigitalInput(0);
+	public static DigitalInput bottomSwitch = new DigitalInput(1);
+	public static Encoder elevatorEncoder = new Encoder(1,2,false,EncodingType.k2X);
     
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
