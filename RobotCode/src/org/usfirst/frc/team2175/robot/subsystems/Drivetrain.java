6c612e9e39a27d0a6ac9502dea5515168627235d
Steve Quintana <squintana32@gmail.com>
2015-01-18 18:11:33 -0600

    Updated DriveTrain Mk.2

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/Drivetrain.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/Drivetrain.java
@@ -7,61 +7,58 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-
 /**
  *
  */
 public class Drivetrain extends Subsystem {
-	
+
 	private Talon leftTalon;
 	private Talon rightTalon;
 	public Encoder leftEncoder;
 	public Encoder rightEncoder;
-	private Gyro gyro;
+	public Gyro gyro;
 	private RobotDrive drivetrain;
-	
+
 	public Drivetrain() {
 		drivetrain = new RobotDrive(1, 2, 3, 4);
-		rightEncoder = new Encoder(1, 2, false, edu.wpi.first.wpilibj.CounterBase.EncodingType.k4X);
-		leftEncoder = new Encoder (1, 2, false, edu.wpi.first.wpilibj.CounterBase.EncodingType.k4X);
+		rightEncoder = new Encoder(1, 2, false,
+				edu.wpi.first.wpilibj.CounterBase.EncodingType.k4X);
+		leftEncoder = new Encoder(1, 2, false,
+				edu.wpi.first.wpilibj.CounterBase.EncodingType.k4X);
 		gyro = new Gyro(0);
-				
+
 	}
-	
-	public void resetEncoders(){
+
+	public void resetEncoders() {
 		leftEncoder.reset();
 		rightEncoder.reset();
 	}
-	
-	
-	
-	public double getMeanEncoderRate(){
+
+	public double getMeanEncoderRate() {
 		return (leftEncoder.getRate() + rightEncoder.getRate()) / 2;
-		}
-	public double getMeanEncoderDistance(){
+	}
+
+	public double getMeanEncoderDistance() {
 		return (leftEncoder.getDistance() + rightEncoder.getDistance()) / 2;
-	
-	public void getGyro(){
-		gyro.getAngle();
-		gyro.getRate();
-		// I'm not sure what you meant with "Get Gyro," so I put in these.
 	}
-		
-	public void tankDrive(){
-		
-	}
-	
-	public void arcadeDrive(){
-		
-	}
-	
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	
+
+	public void tankDrive(double leftSpeed, double rightSpeed) {
+		drivetrain.tankDrive(leftSpeed, rightSpeed);
+
+	}
+
+	public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+		drivetrain.arcadeDrive(moveSpeed, rotateSpeed);
+
+	}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
-
