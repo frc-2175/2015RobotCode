94994028c9ca7a0746492ae15489e47b84c3df82
Nicholas Tourville <nicholastourvlle@gmail.com>
2015-01-20 19:04:48 -0600

    forgot to save before i commited...

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/containerElevator.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/containerElevator.java
@@ -1,9 +1,8 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
-import com.sun.xml.internal.fastinfoset.Encoder;
-
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,24 +10,38 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class containerElevator extends Subsystem {
-	private DigitalInput topSwitch;
-	private DigitalInput bottomSwitch;
-	private Talon elevatorMotor;
-	private Encoder elevatorEncoder;
+	public DigitalInput containerTopSwitch;
+	public DigitalInput containerBottomSwitch;
+	public Talon containerElevatorMotor;
+	public Encoder containerElevatorEncoder;
 
-	public ContainerElevator() {
-		topSwitch = new DigitalInput(3);
-		bottomSwitch = new DigitalInput(4);
-		
-		elevatorMotor = new Talon(0);
-		
-		elevatorEncoder = new Encoder(3, 4, false, EncodingType.k4X);
-		elevatorEncoder.setDistancePerPulse(0);
-		elevatorEncoder.setReverseDirection(false);
-		
+	public containerElevator() {
+		containerTopSwitch = new DigitalInput(3);
+		containerBottomSwitch = new DigitalInput(4);
+
+		containerElevatorMotor = new Talon(0);
+
+		containerElevatorEncoder = new Encoder(3, 4, false, EncodingType.k4X);
+		containerElevatorEncoder.setDistancePerPulse(0);
+		containerElevatorEncoder.setReverseDirection(false);
 
 	}
 
+	public boolean containerIsAtTop(){
+		return containerTopSwitch.get();
+	}
+	public boolean containerIsAtBottom(){
+		return containerBottomSwitch.get();
+	}
+	public void setContainerSpeed(double containerSpeed){
+		containerElevatorMotor.set(containerSpeed);
+	}
+	public double getContainerHeight(){
+		return containerElevatorEncoder.get();
+	}
+	public void resetElevatorEncoder(){
+		containerElevatorEncoder.reset();
+	}
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
