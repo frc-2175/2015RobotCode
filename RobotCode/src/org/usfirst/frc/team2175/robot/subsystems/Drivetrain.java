03858657bbad56f9b15b41215b2e58df0520f254
Steve Quintana <squintana32@gmail.com>
2015-01-21 19:52:37 -0600

    added new things

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/Drivetrain.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/Drivetrain.java
@@ -1,12 +1,7 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
 import org.usfirst.frc.team2175.robot.RobotMap;
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.RobotDrive;
-=======
->>>>>>> Fixed my non-robotmap drivetrain code to include ref to RM
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,37 +9,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends Subsystem {
 
-<<<<<<< HEAD
-//	private Talon leftTalon;
-//	private Talon rightTalon;
-//	public Encoder leftEncoder;
-//	public Encoder rightEncoder;
-//	public Gyro gyro;
-//	private RobotDrive drivetrain;
-
-=======
->>>>>>> Fixed my non-robotmap drivetrain code to include ref to RM
 	public void resetEncoders() {
 		RobotMap.leftEncoder.reset();
 		RobotMap.rightEncoder.reset();
 	}
 
 	public double getMeanEncoderRate() {
-<<<<<<< HEAD
 		return (RobotMap.leftEncoder.getRate() + RobotMap.rightEncoder.getRate()) / 2;
 	}
 
 	public double getMeanEncoderDistance() {
 		return (RobotMap.leftEncoder.getDistance() + RobotMap.rightEncoder.getDistance()) / 2;
-=======
-		return (RobotMap.leftEncoder.getRate() + RobotMap.rightEncoder
-				.getRate()) / 2;
+		return (RobotMap.leftEncoder.getRate() + RobotMap.rightEncoder.getRate()) / 2;
 	}
 
 	public double getMeanEncoderDistance() {
-		return (RobotMap.leftEncoder.getDistance() + RobotMap.rightEncoder
-				.getDistance()) / 2;
->>>>>>> Fixed my non-robotmap drivetrain code to include ref to RM
+		return (RobotMap.leftEncoder.getDistance() + RobotMap.rightEncoder.getDistance()) / 2;
 	}
 
 	public void tankDrive(double leftSpeed, double rightSpeed) {
