214acff855cdd86f9eacbcbf99a1e85866a32ca5
mabster314 <mabster314@gmail.com>
2015-01-22 19:15:11 -0600

    Rewrote MoveElevatorToPosition

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/MoveElevatorToPosition.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/MoveElevatorToPosition.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class moveElevatorToPosition1 extends Command {
+public class MoveElevatorToPosition extends Command {
 
-    public moveElevatorToPosition1() {
+	double position;
+    public MoveElevatorToPosition(double position) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.pidToteElevator);
+    	this.position = position;
     }
 
     // Called just before this Command runs the first time
@@ -21,7 +23,7 @@ public class moveElevatorToPosition1 extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pidToteElevator.setSetpoint(100); //TODO figure out distance needed
+    	Robot.pidToteElevator.setSetpoint(position);
     }
 
     // Make this return true when this Command no longer needs to run execute()
