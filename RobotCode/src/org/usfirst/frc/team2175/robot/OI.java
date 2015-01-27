fb0a2aedb61f017a3880d126b438e1ae0f4f11a7
Nicholas Tourville <nicholastourvlle@gmail.com>
2015-01-26 20:07:16 -0600

    put generic buttons into OI with easy renaming

--- RobotCode/src/org/usfirst/frc/team2175/robot/OI.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/OI.java
@@ -40,6 +40,38 @@ public class OI {
 	public Joystick gamepad;
 	public JoystickButton precisionMode;
 	public JoystickButton shifters;
+	public JoystickButton left2 = new JoystickButton(leftStick, 2);
+	public JoystickButton left3 = new JoystickButton(leftStick, 3);
+	public JoystickButton left4 = new JoystickButton(leftStick, 4);
+	public JoystickButton left5 = new JoystickButton(leftStick, 5);
+	public JoystickButton left6 = new JoystickButton(leftStick, 6);
+	public JoystickButton left7 = new JoystickButton(leftStick, 7);
+	public JoystickButton left8 = new JoystickButton(leftStick, 8);
+	public JoystickButton left9 = new JoystickButton(leftStick, 9);
+	public JoystickButton left10 = new JoystickButton(leftStick, 10);
+	public JoystickButton right2 = new JoystickButton(rightStick, 2);
+	public JoystickButton right3 = new JoystickButton(rightStick, 3);
+	public JoystickButton right4 = new JoystickButton(rightStick, 4);
+	public JoystickButton right5 = new JoystickButton(rightStick, 5);
+	public JoystickButton right6 = new JoystickButton(rightStick, 6);
+	public JoystickButton right7 = new JoystickButton(rightStick, 7);
+	public JoystickButton right8 = new JoystickButton(rightStick, 8);
+	public JoystickButton right9 = new JoystickButton(rightStick, 9);
+	public JoystickButton right10 = new JoystickButton(rightStick, 10);
+	public JoystickButton gamepad1 = new JoystickButton(gamepad, 1);
+	public JoystickButton gamepad2 = new JoystickButton(gamepad, 2);
+	public JoystickButton gamepad3 = new JoystickButton(gamepad, 3);
+	public JoystickButton gamepad4 = new JoystickButton(gamepad, 4);
+	public JoystickButton gamepad5 = new JoystickButton(gamepad, 5);
+	public JoystickButton gamepad6 = new JoystickButton(gamepad, 6);
+	public JoystickButton gamepad7 = new JoystickButton(gamepad, 7);
+	public JoystickButton gamepad8 = new JoystickButton(gamepad, 8);
+	public JoystickButton gamepad9 = new JoystickButton(gamepad, 9);
+	public JoystickButton gamepad10 = new JoystickButton(gamepad, 10);
+
+
+
+
 
 	public double deadbandValue = 0.05;
 	public OI() {
@@ -48,6 +80,10 @@ public class OI {
 		gamepad = new Joystick(2);
 		precisionMode = new JoystickButton(rightStick, 1);
 		shifters = new JoystickButton(leftStick, 1);
+		
+
+
+
 	}
 
 	public double getMoveValue() {
