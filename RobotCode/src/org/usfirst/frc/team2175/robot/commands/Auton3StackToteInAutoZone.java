8fd8a2534254566d6409051e146097e475a970db
Nicholas Tourville <nicholastourvlle@gmail.com>
2015-01-28 17:29:41 -0600

    renamed Auto3StackTote to Auton3StackTote

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/Auton3StackToteInAutoZone.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/Auton3StackToteInAutoZone.java
@@ -6,9 +6,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 //TODO Finish this, requires other things first
-public class Auto3StackToteInAutoZone extends CommandGroup {
+public class Auton3StackToteInAutoZone extends CommandGroup {
     
-    public  Auto3StackToteInAutoZone() {
+    public  Auton3StackToteInAutoZone() {
        //Would start in same spot as auto1Drive forward and left, needs more testing on getting to the required location 
       //This is currently only just moving it the same spot from Auton Drive forward
     	addSequential(new ArcadeDriveWithInputs(0.5, 0), 3);
