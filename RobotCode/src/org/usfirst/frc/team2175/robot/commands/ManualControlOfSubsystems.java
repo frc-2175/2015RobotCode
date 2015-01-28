0659cd55247135821bebb8b123fff984fa095be2
Nicholas Tourville <nicholastourvlle@gmail.com>
2015-01-27 18:04:14 -0600

    Deleted Auton2Push1Tote and ManualControlOfSubsystems and changed them to command groups

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/ManualControlOfSubsystems.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/ManualControlOfSubsystems.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 /**
  *
  */
-public class Auton2Push1Tote extends CommandGroup {
+public class ManualControlOfSubsystems extends CommandGroup {
     
-    public  Auton2Push1Tote() {
+    public  ManualControlOfSubsystems() {
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
