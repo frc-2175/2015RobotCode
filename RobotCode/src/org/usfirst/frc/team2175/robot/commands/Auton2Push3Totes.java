1c496c6310ce7a78780aecd73ef4041da69198c2
Jacob Jensen <jjja380@gmail.com>
2015-01-28 19:59:10 -0600

    Removed Green Text.

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/Auton2Push3Totes.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/Auton2Push3Totes.java
@@ -22,21 +22,6 @@ public class Auton2Push3Totes extends CommandGroup {
     	addSequential(new ArcadeDriveWithInputs(1,0),3);
     	addSequential(new ArcadeDriveWithInputs(0,1),2);
     	addSequential(new ArcadeDriveWithInputs(1,0),5);
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+     
     }
 }
