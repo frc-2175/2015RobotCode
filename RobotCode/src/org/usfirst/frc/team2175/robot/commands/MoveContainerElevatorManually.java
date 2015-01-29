8ca00e863b82fc2aaa161f75628af31170f49a4a
Owen Hoffend <mcnewomaster@gmail.com>
2015-01-29 11:04:38 -0600

    Implemented PID for the ContainerElevator subsystem. Fixed a small push conflict in drivetrain from somewhere.

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/MoveContainerElevatorManually.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/MoveContainerElevatorManually.java
@@ -20,7 +20,7 @@ public class MoveContainerElevatorManually extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	double elevatorSpeed = Robot.oi.gamepad.getRawAxis(3);
-    	Robot.containerElevator.setContainerSpeed(elevatorSpeed);
+    	Robot.containerElevator.setContainerElevatorSpeed(elevatorSpeed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerElevator.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerElevator.java
@@ -1,37 +1,65 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
 import org.usfirst.frc.team2175.robot.RobotMap;
-
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class ContainerElevator extends Subsystem {
-   
+	
+	public PIDController containerElevatorController;
+	
+	private class ContainerElevatorControllerHandler implements PIDSource, PIDOutput {
+
+		@Override
+		public void pidWrite(double output) {
+			RobotMap.containerElevatorMotor.set(output);
+		}
+
+		@Override
+		public double pidGet() {
+			return getContainerHeight(); 
+		}
+	}
 
     public ContainerElevator() {
-        RobotMap.containerElevatorEncoder.setDistancePerPulse(0);
+    	ContainerElevatorControllerHandler containerElevatorControllerHandler = new ContainerElevatorControllerHandler();
+    	containerElevatorController = new PIDController(0, 0, 0, containerElevatorControllerHandler, containerElevatorControllerHandler); 
+    	//TODO determine PID constants
+    	containerElevatorController.setAbsoluteTolerance(0.5);
+    	
+        RobotMap.containerElevatorEncoder.setDistancePerPulse(0); //TODO determine distance per pulse and direction of encoder
         RobotMap.containerElevatorEncoder.setReverseDirection(false);
-
     }
 
-    public boolean containerIsAtTop(){
+    public boolean containerElevatorIsAtTop(){
         return RobotMap.containerTopSwitch.get();
     }
-    public boolean containerIsAtBottom(){
+    
+    public boolean containerElevatorIsAtBottom(){
         return RobotMap.containerBottomSwitch.get();
     }
-    public void setContainerSpeed(double containerSpeed){
-        RobotMap.containerElevatorMotor.set(containerSpeed);
+    
+    public void setContainerElevatorSpeed(double containerSpeed){
+    	if(containerElevatorIsAtTop() && RobotMap.containerElevatorMotor.getSpeed() > 0){
+    		RobotMap.containerElevatorMotor.set(0);
+    	}
+    	else if(containerElevatorIsAtBottom() && RobotMap.containerElevatorMotor.getSpeed() < 0){
+    		RobotMap.containerElevatorMotor.set(0);
+    	}
+    	else{
+    		RobotMap.containerElevatorMotor.set(containerSpeed);
+    	}
     }
+    
     public double getContainerHeight(){
-        return RobotMap.containerElevatorEncoder.get();
+        return RobotMap.containerElevatorEncoder.getDistance();
     }
+    
     public void resetElevatorEncoder(){
         RobotMap.containerElevatorEncoder.reset();
     }
