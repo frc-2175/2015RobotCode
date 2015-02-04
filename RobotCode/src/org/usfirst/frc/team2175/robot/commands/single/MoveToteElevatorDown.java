9f93bb658e65dca1097967d6dcd285e7ee328884
Steve Quintana <squintana32@gmail.com>
2015-02-03 18:03:00 -0600

    Created "MoveToteElevatorDown" command.

---
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/single/MoveToteElevatorDown.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team2175.robot.commands.single;
+
+import org.usfirst.frc.team2175.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+/**
+ *
+ */
+public class MoveToteElevatorDown extends Command {
+
+    public MoveToteElevatorDown() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	double currentLevel = Robot.toteElevator.getHeight();
+    	double newLevel = Robot.properties.toteConfig.getNextLevelDown(currentLevel);
+    	MoveToteElevatorToPosition command = new MoveToteElevatorToPosition(newLevel);
+    	Scheduler.getInstance().add(command);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
