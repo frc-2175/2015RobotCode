5dfb4b3f89d0a4d01bdb27caf888fbd5f1d96fb4
Jeff Jensen <jjensen@apache.org>
2015-02-07 16:32:43 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/OI.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/OI.java
@@ -10,82 +10,86 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	// // CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+    // // CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a
+    // joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
 
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
 
-	// // TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
+    // // TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
 
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
 
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
 
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
-	public Joystick leftStick;
-	public Joystick rightStick;
-	public Joystick gamepad;
-	public JoystickButton precisionMode;
-	public JoystickButton shifters;
+    // Start the command when the button is released and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
+    public Joystick leftStick;
+    public Joystick rightStick;
+    public Joystick gamepad;
+    public JoystickButton precisionMode;
+    public JoystickButton shifters;
 
-	public double deadbandValue;
+    public double deadbandValue;
 
-	public OI() {
-		leftStick = new Joystick(0);
-		rightStick = new Joystick(1);
-		gamepad = new Joystick(2);
-		
-		precisionMode = new JoystickButton(rightStick, 1);
-		shifters = new JoystickButton(leftStick, 1);
-		deadbandValue = Robot.properties.getDeadbandSize();
-		
-		JoystickButton gamepad1 = new JoystickButton(gamepad, 1);
-		JoystickButton gamepad2 = new JoystickButton(gamepad, 2);
-		JoystickButton gamepad3 = new JoystickButton(gamepad, 3);
-		JoystickButton gamepad4 = new JoystickButton(gamepad, 4);
-		
-		gamepad1.whenPressed(new MoveToteElevatorToPosition(Robot.properties.toteConfig.pickup));
-		gamepad2.whenPressed(new MoveToteElevatorToPosition(Robot.properties.toteConfig.driving));
-		gamepad3.whenPressed(new MoveToteElevatorToPosition(Robot.properties.toteConfig.step));
-		gamepad4.whenPressed(new MoveToteElevatorToPosition(Robot.properties.toteConfig.stack));
-	}
+    public OI() {
+        leftStick = new Joystick(0);
+        rightStick = new Joystick(1);
+        gamepad = new Joystick(2);
 
-	public double getMoveValue() {
-		if (!precisionMode.get()) {
-			return handleDeadband(leftStick.getY());
-		} else {
-			return handleDeadband(leftStick.getY() * 0.5);
-		}
-	}
+        precisionMode = new JoystickButton(rightStick, 1);
+        shifters = new JoystickButton(leftStick, 1);
+        deadbandValue = Robot.properties.getDeadbandSize();
 
-	public double getTurnValue() {
-		if (!precisionMode.get()) {
-			return handleDeadband(rightStick.getX());
-		} else {
-			return handleDeadband(rightStick.getX() * 0.5);
-		}
-	}
+        JoystickButton gamepad1 = new JoystickButton(gamepad, 1);
+        JoystickButton gamepad2 = new JoystickButton(gamepad, 2);
+        JoystickButton gamepad3 = new JoystickButton(gamepad, 3);
+        JoystickButton gamepad4 = new JoystickButton(gamepad, 4);
 
-	public double handleDeadband(double input) {
-		if (input <= deadbandValue) {
-			return 0;
-		} else {
-			return input;
-		}
-	}
+        gamepad1.whenPressed(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.pickup));
+        gamepad2.whenPressed(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.driving));
+        gamepad3.whenPressed(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.step));
+        gamepad4.whenPressed(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.stack));
+    }
+
+    public double getMoveValue() {
+        if (!precisionMode.get()) {
+            return handleDeadband(leftStick.getY());
+        } else {
+            return handleDeadband(leftStick.getY() * 0.5);
+        }
+    }
+
+    public double getTurnValue() {
+        if (!precisionMode.get()) {
+            return handleDeadband(rightStick.getX());
+        } else {
+            return handleDeadband(rightStick.getX() * 0.5);
+        }
+    }
+
+    public double handleDeadband(double input) {
+        if (input <= deadbandValue) {
+            return 0;
+        } else {
+            return input;
+        }
+    }
 }
