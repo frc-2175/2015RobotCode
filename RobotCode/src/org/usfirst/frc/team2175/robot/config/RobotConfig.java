5a77fce28331143045cef8d2dcca8ac10e99cbbc
Jeff Jensen <jjensen@apache.org>
2015-02-07 14:50:21 -0600

    Log not-found property before throwing exception.

--- RobotCode/src/org/usfirst/frc/team2175/robot/config/RobotConfig.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/config/RobotConfig.java
@@ -131,8 +131,10 @@ public class RobotConfig {
     protected String getPropertyValue(String propertyName, Properties props) {
         String value = props.getProperty(propertyName);
         if (value == null) {
-            throw new IllegalStateException("Property '" + propertyName
-                    + "' not found in property file");
+            String msg = "Property '" + propertyName
+                    + "' not found in property file";
+            log.severe(msg);
+            throw new IllegalStateException(msg);
         }
         return value;
     }
