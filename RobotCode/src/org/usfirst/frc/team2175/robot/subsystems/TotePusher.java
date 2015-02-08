eb3168fb0edfb533e427bcb97d8e3d000f61b79c
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:53:06 -0600

    Add some logging to TotePusher.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/TotePusher.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/TotePusher.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
+import java.util.logging.Logger;
+
 import org.usfirst.frc.team2175.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,25 +10,35 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class TotePusher extends Subsystem {
-    public void setMotorSpeed(double armspeed) {
-        if (isPusherExtended() && armspeed >= 0) {
-            RobotMap.totePusherArm.set(0);
+    private final Logger log = Logger.getLogger(getClass().getName());
+
+    public void setMotorSpeed(double armSpeed) {
+        double newSpeed;
+        if (isPusherExtended() && armSpeed >= 0) {
+            newSpeed = 0;
         }
-        if (isPusherRetracted() && armspeed <= 0) {
-            RobotMap.totePusherArm.set(0);
+        // FIXME is this an else if?
+        if (isPusherRetracted() && armSpeed <= 0) {
+            newSpeed = 0;
         } else {
-            RobotMap.totePusherArm.set(armspeed);
+            newSpeed = armSpeed;
         }
+        RobotMap.totePusherArm.set(newSpeed);
+        log.fine("requested armSpeed=" + armSpeed + ", newSpeed=" + newSpeed);
     }
 
     public boolean isPusherExtended() {
-        return RobotMap.pusherSwitch.get();
+        boolean isExtended = RobotMap.pusherSwitch.get();
+        log.fine("isExtended=" + isExtended);
+        return isExtended;
     }
 
     // TODO change these two into one sensor boolean
 
     public boolean isPusherRetracted() {
-        return RobotMap.pusherSwitch.get();
+        boolean isRetracted = RobotMap.pusherSwitch.get();
+        log.fine("isRetracted=" + isRetracted);
+        return isRetracted;
     }
 
     @Override
