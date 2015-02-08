3532b367be611301a56b3d73890cd46230c869b6
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:24:00 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteElevator.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteElevator.java
@@ -12,77 +12,74 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ToteElevator extends Subsystem {
+    public PIDController heightController;
 
-	protected void usePIDOutput(double output) {
-		// Use output to drive your system, like a motor
-		// e.g. yourMotor.set(output);
+    protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
 
-		RobotMap.toteElevatorTalon.set(output);
-	}
+        RobotMap.toteElevatorTalon.set(output);
+    }
 
-	public PIDController heightController;
+    private class HeightControllerHandler implements PIDSource, PIDOutput {
+        @Override
+        public void pidWrite(double output) {
+            // Do something with the output PID value, like update motors
+            RobotMap.toteElevatorTalon.set(output);
+        }
 
-	private class HeightControllerHandler implements PIDSource, PIDOutput {
+        @Override
+        public double pidGet() {
+            // Return the sensor value for the PID input
+            return RobotMap.toteElevatorEncoder.getDistance();
+        }
+    }
 
-		@Override
-		public void pidWrite(double output) {
-			// Do something with the output PID value, like update motors
-			RobotMap.toteElevatorTalon.set(output);
-		}
+    public ToteElevator() {
+        // TODO Find actual distance per pulse
+        RobotMap.toteElevatorEncoder.setDistancePerPulse(1 / 50.8);
+        RobotMap.toteElevatorEncoder.setReverseDirection(true);
+        HeightControllerHandler heightHandler = new HeightControllerHandler();
+        heightController = new PIDController(
+                Robot.properties.getToteElevatorP(),
+                Robot.properties.getToteElevatorI(),
+                Robot.properties.getToteElevatorD(), heightHandler,
+                heightHandler);
+        heightController.setOutputRange(-0.2, 1);
+        heightController.setAbsoluteTolerance(.05);
+        // Add other encoder-related initializations here if needed.
+    }
 
-		@Override
-		public double pidGet() {
-			// Return the sensor value for the PID input
-			return RobotMap.toteElevatorEncoder.getDistance();
-		}
-	}
+    public boolean isAtBottom() {
+        return RobotMap.toteSwitch.get();
+    }
 
-	public ToteElevator() {
-		// TODO Find actual distance per pulse
-		RobotMap.toteElevatorEncoder.setDistancePerPulse(1/50.8);
-		RobotMap.toteElevatorEncoder.setReverseDirection(true);
-		HeightControllerHandler heightHandler = new HeightControllerHandler();
-		heightController = new PIDController(
-				Robot.properties.getToteElevatorP(),
-				Robot.properties.getToteElevatorI(),
-				Robot.properties.getToteElevatorD(), heightHandler,
-				heightHandler);
-		heightController.setOutputRange(-0.2, 1);
-		heightController.setAbsoluteTolerance(.05);
-		// Add other encoder-related initializations here if needed.
-	}
+    // TODO change these two sensors to one boolean
 
-	public boolean isAtBottom() {
-		return RobotMap.toteSwitch.get();
-	}
-	
-	//TODO change these two sensors to one boolean
+    public boolean isAtTop() {
+        return RobotMap.toteSwitch.get();
+    }
 
-	public boolean isAtTop() {
-		return RobotMap.toteSwitch.get();
-	}
+    public void setSpeed(double toteElevatorSpeed) {
+        if (isAtTop() && toteElevatorSpeed > 0) {
+            RobotMap.toteElevatorTalon.set(0);
+        } else if (isAtBottom() && toteElevatorSpeed < 0) {
+            RobotMap.toteElevatorTalon.set(0);
+        } else {
+            RobotMap.toteElevatorTalon.set(toteElevatorSpeed);
+        }
+    }
 
-	public void setSpeed(double toteElevatorSpeed) {
-		if (isAtTop() && toteElevatorSpeed > 0) {
-			RobotMap.toteElevatorTalon.set(0);
-		} else if (isAtBottom() && toteElevatorSpeed < 0) {
-			RobotMap.toteElevatorTalon.set(0);
-		} else {
-			RobotMap.toteElevatorTalon.set(toteElevatorSpeed);
-		}
-	}
+    public void resetEncoder() {
+        RobotMap.toteElevatorEncoder.reset();
+    }
 
-	public void resetEncoder() {
-		RobotMap.toteElevatorEncoder.reset();
-	}
+    public double getHeight() {
+        return RobotMap.toteElevatorEncoder.getDistance();
+    }
 
-	public double getHeight() {
-		return RobotMap.toteElevatorEncoder.getDistance();
-	}
-
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-
-	}
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+    }
 }
