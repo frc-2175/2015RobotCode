a3c1dcdbd85cc16ea3491edd280517dc887c588d
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:41:47 -0600

    Add some logging to ToteIntake.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteIntake.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteIntake.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
+import java.util.logging.Logger;
+
 import org.usfirst.frc.team2175.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,17 +10,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ToteIntake extends Subsystem {
+    private final Logger log = Logger.getLogger(getClass().getName());
+
     public void setWheelSpeed(double wheelSpeed) {
+        log.fine("wheelSpeed=" + wheelSpeed);
         RobotMap.toteIntakeWheelMotor.set(wheelSpeed);
     }
 
-    public void setIntakeArms(boolean in) {
-        RobotMap.toteIntakeArms.set(in);
+    public void setIntakeArms(boolean on) {
+        log.fine("on=" + on);
+        RobotMap.toteIntakeArms.set(on);
     }
 
     @Override
     protected void initDefaultCommand() {
-        // TODO Auto-generated method stub
-
     }
 }
