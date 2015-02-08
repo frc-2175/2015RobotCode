886079d8c98dde656c5c1c0e5eab7e804f553dcf
Jeff Jensen <jjensen@apache.org>
2015-02-07 20:10:44 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerElevator.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerElevator.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
 import org.usfirst.frc.team2175.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
@@ -10,64 +11,68 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ContainerElevator extends Subsystem {
-	
-	public PIDController containerElevatorController;
-	
-	private class ContainerElevatorControllerHandler implements PIDSource, PIDOutput {
+    public PIDController containerElevatorController;
 
-		@Override
-		public void pidWrite(double output) {
-			RobotMap.containerElevatorMotor.set(output);
-		}
+    private class ContainerElevatorControllerHandler implements PIDSource,
+            PIDOutput {
+        @Override
+        public void pidWrite(double output) {
+            RobotMap.containerElevatorMotor.set(output);
+        }
 
-		@Override
-		public double pidGet() {
-			return getContainerHeight(); 
-		}
-	}
+        @Override
+        public double pidGet() {
+            return getContainerHeight();
+        }
+    }
 
     public ContainerElevator() {
-    	ContainerElevatorControllerHandler containerElevatorControllerHandler = new ContainerElevatorControllerHandler();
-    	containerElevatorController = new PIDController(0, 0, 0, containerElevatorControllerHandler, containerElevatorControllerHandler); 
-    	//TODO determine PID constants
-    	containerElevatorController.setAbsoluteTolerance(0.5);
-    	
-        RobotMap.containerElevatorEncoder.setDistancePerPulse(0); //TODO determine distance per pulse and direction of encoder
+        ContainerElevatorControllerHandler containerElevatorControllerHandler = new ContainerElevatorControllerHandler();
+        containerElevatorController = new PIDController(0, 0, 0,
+                containerElevatorControllerHandler,
+                containerElevatorControllerHandler);
+        // TODO determine PID constants
+        containerElevatorController.setAbsoluteTolerance(0.5);
+
+        // TODO determine distance per pulse and direction of encoder
+        RobotMap.containerElevatorEncoder.setDistancePerPulse(0);
         RobotMap.containerElevatorEncoder.setReverseDirection(false);
     }
 
-    public boolean containerElevatorIsAtTop(){
+    public boolean containerElevatorIsAtTop() {
         return RobotMap.containerSwitch.get();
     }
-    
-    //TODO change these two to one sensor boolean
-    
-    public boolean containerElevatorIsAtBottom(){
+
+    // TODO change these two to one sensor boolean
+
+    public boolean containerElevatorIsAtBottom() {
         return RobotMap.containerSwitch.get();
     }
-    
-    public void setContainerElevatorSpeed(double containerSpeed){
-    	if(containerElevatorIsAtTop() && RobotMap.containerElevatorMotor.getSpeed() > 0){
-    		RobotMap.containerElevatorMotor.set(0);
-    	}
-    	else if(containerElevatorIsAtBottom() && RobotMap.containerElevatorMotor.getSpeed() < 0){
-    		RobotMap.containerElevatorMotor.set(0);
-    	}
-    	else{
-    		RobotMap.containerElevatorMotor.set(containerSpeed);
-    	}
+
+    public void setContainerElevatorSpeed(double containerSpeed) {
+        if (containerElevatorIsAtTop()
+                && RobotMap.containerElevatorMotor.getSpeed() > 0) {
+            RobotMap.containerElevatorMotor.set(0);
+        } else if (containerElevatorIsAtBottom()
+                && RobotMap.containerElevatorMotor.getSpeed() < 0) {
+            RobotMap.containerElevatorMotor.set(0);
+        } else {
+            RobotMap.containerElevatorMotor.set(containerSpeed);
+        }
     }
-    
-    public double getContainerHeight(){
+
+    public double getContainerHeight() {
         return RobotMap.containerElevatorEncoder.getDistance();
     }
-    
-    public void resetElevatorEncoder(){
+
+    public void resetElevatorEncoder() {
         RobotMap.containerElevatorEncoder.reset();
     }
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    @Override
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
