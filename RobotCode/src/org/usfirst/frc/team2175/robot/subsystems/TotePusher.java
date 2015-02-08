96520bfd1743c42a659e115216af454b3d71fcc4
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:45:53 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/TotePusher.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/TotePusher.java
@@ -1,37 +1,35 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
 import org.usfirst.frc.team2175.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 /**
  *
  */
-public class TotePusher extends Subsystem{
-    
-	public void setMotorSpeed(double armspeed){
-		if(isPusherExtended()&&armspeed>=0) {
-			RobotMap.totePusherArm.set(0);
-		} if(isPusherRetracted()&&armspeed<=0){
-			RobotMap.totePusherArm.set(0);
-		}else{
-			RobotMap.totePusherArm.set(armspeed);
-		}
-	}
-	
-	public boolean isPusherExtended(){
-		return RobotMap.pusherSwitch.get();
-	}
-	
-	//TODO change these two into one sensor boolean
-	
-	public boolean isPusherRetracted(){
-		return RobotMap.pusherSwitch.get();
-	}
-	
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
-    
-}
+public class TotePusher extends Subsystem {
+    public void setMotorSpeed(double armspeed) {
+        if (isPusherExtended() && armspeed >= 0) {
+            RobotMap.totePusherArm.set(0);
+        }
+        if (isPusherRetracted() && armspeed <= 0) {
+            RobotMap.totePusherArm.set(0);
+        } else {
+            RobotMap.totePusherArm.set(armspeed);
+        }
+    }
 
+    public boolean isPusherExtended() {
+        return RobotMap.pusherSwitch.get();
+    }
+
+    // TODO change these two into one sensor boolean
+
+    public boolean isPusherRetracted() {
+        return RobotMap.pusherSwitch.get();
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+    }
+}
