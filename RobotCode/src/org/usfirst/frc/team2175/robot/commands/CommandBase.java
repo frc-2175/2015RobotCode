b161e2d1241f5a369eae154a9e90d53e27c408ea
Jeff Jensen <jjensen@apache.org>
2015-02-08 14:32:44 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/CommandBase.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/CommandBase.java
@@ -2,21 +2,18 @@ package org.usfirst.frc.team2175.robot.commands;
 
 import java.util.logging.Logger;
 
-import org.usfirst.frc.team2175.robot.OI;
-import org.usfirst.frc.team2175.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 public abstract class CommandBase extends Command {
-	
-	protected final Logger log = Logger.getLogger(getClass().getName());
-	
-	protected void initialize() {
-		log.info("Starting command");
-	}
-	
-	protected void end() {
-		log.info("Ending command");
-	}
-	
+    protected final Logger log = Logger.getLogger(getClass().getName());
+
+    @Override
+    protected void initialize() {
+        log.info("Starting command");
+    }
+
+    @Override
+    protected void end() {
+        log.info("Ending command");
+    }
 }
