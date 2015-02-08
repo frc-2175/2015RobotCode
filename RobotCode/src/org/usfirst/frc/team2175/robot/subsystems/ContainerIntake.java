2d6265450dc5c999051de9f876d1c815ad9cd08c
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:04:22 -0600

    Add some logging to ContainerIntake.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerIntake.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ContainerIntake.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
+import java.util.logging.Logger;
+
 import org.usfirst.frc.team2175.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,11 +10,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ContainerIntake extends Subsystem {
+    private final Logger log = Logger.getLogger(getClass().getName());
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void setIntakeArms(boolean on) {
+        log.fine("on=" + on);
         RobotMap.ContainerIntakeArms.set(on);
     }
 
