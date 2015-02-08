05bd91d08347a40c16708e9cdd4904bca83b5a6a
Jeff Jensen <jjensen@apache.org>
2015-02-07 21:38:51 -0600

    Reformat only, no functional change.

--- RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteIntake.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/subsystems/ToteIntake.java
@@ -1,26 +1,24 @@
 package org.usfirst.frc.team2175.robot.subsystems;
 
 import org.usfirst.frc.team2175.robot.RobotMap;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class ToteIntake extends Subsystem{
-	
-	public void setWheelSpeed(double wheelSpeed) {
-		RobotMap.toteIntakeWheelMotor.set(wheelSpeed);
-	}
-	
-	public void setIntakeArms(boolean in){
-		RobotMap.toteIntakeArms.set(in);
-	}
+public class ToteIntake extends Subsystem {
+    public void setWheelSpeed(double wheelSpeed) {
+        RobotMap.toteIntakeWheelMotor.set(wheelSpeed);
+    }
 
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    public void setIntakeArms(boolean in) {
+        RobotMap.toteIntakeArms.set(in);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
 }
