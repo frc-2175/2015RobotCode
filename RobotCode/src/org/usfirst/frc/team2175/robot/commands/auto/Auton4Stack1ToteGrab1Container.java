a143fc405499d95a582c8cbd71fcd14288d827f6
Jacob Jensen <jjja380@gmail.com>
2015-02-10 21:41:13 -0600

    Finished Auton4Stack1ToteGrab1Container

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/Auton4Stack1ToteGrab1Container.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/Auton4Stack1ToteGrab1Container.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team2175.robot.commands.auto;
 
+import org.usfirst.frc.team2175.robot.commands.groups.IntakeTote;
+import org.usfirst.frc.team2175.robot.commands.groups.StackTote;
+import org.usfirst.frc.team2175.robot.commands.single.DriveInches;
+import org.usfirst.frc.team2175.robot.commands.single.OpenContainerIntake;
+import org.usfirst.frc.team2175.robot.commands.single.TurnDegrees;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,7 +14,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Auton4Stack1ToteGrab1Container extends CommandGroup {
     
     public  Auton4Stack1ToteGrab1Container() {
-        
-    	
+    	addParallel(new IntakeTote());
+    	addSequential(new DriveInches(36));
+    	addParallel(new StackTote());
+    	addSequential(new OpenContainerIntake());
+    	addSequential(new DriveInches(-72));
+    	addSequential(new TurnDegrees(90));
+    	addSequential(new DriveInches(108)); //TODO find good number for this
     }
 }
