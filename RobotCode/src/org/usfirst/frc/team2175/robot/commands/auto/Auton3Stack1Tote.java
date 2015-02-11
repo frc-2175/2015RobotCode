d3990d676fd5104e484278211773230df442f3a4
Jacob Jensen <jjja380@gmail.com>
2015-02-10 20:52:11 -0600

    Finished AutonStack1Tote

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/Auton3Stack1Tote.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/Auton3Stack1Tote.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team2175.robot.commands.auto;
 
+import org.usfirst.frc.team2175.robot.commands.groups.IntakeTote;
+import org.usfirst.frc.team2175.robot.commands.groups.StackTote;
 import org.usfirst.frc.team2175.robot.commands.single.ArcadeDriveWithInputs;
+import org.usfirst.frc.team2175.robot.commands.single.DriveInches;
+import org.usfirst.frc.team2175.robot.commands.single.TurnDegrees;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,7 +17,11 @@ public class Auton3Stack1Tote extends CommandGroup {
     public  Auton3Stack1Tote() {
        //Would start in same spot as auto1Drive forward and left, needs more testing on getting to the required location 
       //This is currently only just moving it the same spot from Auton Drive forward
-    	addSequential(new ArcadeDriveWithInputs(0.5, 0), 3);
-    	
+    	addParallel(new IntakeTote());
+    	addSequential(new DriveInches(36));
+    	addSequential(new StackTote());
+    	addSequential(new TurnDegrees(90));
+    	addSequential(new DriveInches(108)); // TODO find good number
+    	   
     }
 }
