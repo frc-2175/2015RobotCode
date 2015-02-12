c33eb53699af0514004e4e6b03339f1e6fc3e592
Jacob Jensen <jjja380@gmail.com>
2015-02-11 20:36:56 -0600

    Moving COntainer elevator

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/single/MoveContainerElevatorDown.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/single/MoveContainerElevatorDown.java
@@ -11,33 +11,39 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 public class MoveContainerElevatorDown extends Command {
 
     public MoveContainerElevatorDown() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        double currentLevel = Robot.containerElevator.getContainerHeight();
+        double newLevel = Robot.properties.containerConfig
+                .getNextLevelDown(currentLevel);
+        MoveContainerElevatorDown command = new MoveContainerElevatorDown();
+        Scheduler.getInstance().add(command);
     }
 
     // Called just before this Command runs the first time
+    @Override
     protected void initialize() {
-    	double currentLevel = Robot.containerElevator.getContainerHeight();
-    	double newLevel = Robot.properties.containerConfig.getNextLevelDown(currentLevel);
-    	MoveContainerElevatorDown command = new MoveContainerElevatorDown();
-    	Scheduler.getInstance().add(command);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
+    @Override
     protected void execute() {
     }
 
     // Make this return true when this Command no longer needs to run execute()
+    @Override
     protected boolean isFinished() {
         return true;
     }
 
     // Called once after isFinished returns true
+    @Override
     protected void end() {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
+    @Override
     protected void interrupted() {
+        end();
     }
 }
