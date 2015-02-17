beab70cfa209ebf93eac6d912974154677554d4c
Jeff Jensen <jjensen@apache.org>
2015-02-16 22:20:57 -0600

    Configure socket logger so driver station can use GUI Lilith log viewer.

--- RobotCode/src/org/usfirst/frc/team2175/robot/config/LoggingConfiguration.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/config/LoggingConfiguration.java
@@ -4,9 +4,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SocketHandler;
+import java.util.logging.XMLFormatter;
 
-public class LoggingConfiguration {
+public class LoggingConfiguration extends AbstractConfig {
     /** Use ROBOT_LOCATION for deployment running. */
     public static final String LOGGING_PROPERTIES_FILE_ROBOT_LOCATION = "/home/lvuser/logging.properties";
 
@@ -17,6 +24,7 @@ public class LoggingConfiguration {
 
     public void initializeLogging() {
         initializeFileLog();
+        initializeSocketLog();
     }
 
     protected void initializeFileLog() {
@@ -38,6 +46,55 @@ public class LoggingConfiguration {
             throw new IllegalStateException(
                     "Unable to read logging properties", e);
         }
+    }
+
+    protected void initializeSocketLog() {
+        final Handler handler = makeSocketHandler();
+
+        if (handler != null) {
+            configureSocketHandler(handler);
+        }
+    }
+
+    protected Handler makeSocketHandler() {
+        final Properties props = new PropertiesLoader()
+                .loadProperties(loggingPropertiesFileToUse);
+
+        final String socketHandlerHostname = getStringPropertyValue(
+                "socket.handler.hostname", props);
+        final int socketHandlerPort = getIntPropertyValue(
+                "socket.handler.port", props);
+
+        Handler handler = null;
+        try {
+            handler = new SocketHandler(socketHandlerHostname,
+                    socketHandlerPort);
+        } catch (IOException e) {
+            final Logger log = Logger.getLogger(getClass().getName());
+            final String msg = "Lilith log viewer not running?"
+                    + " Error instantiating SocketHandler with host="
+                    + socketHandlerHostname + ", socketHandlerPort"
+                    + socketHandlerPort + ", msg=" + e.getClass().getName()
+                    + ": " + e.getMessage();
+            log.info(msg);
+        }
+        return handler;
+    }
+
+    protected void configureSocketHandler(final Handler handler) {
+        final String handlerEncoding = "UTF-8";
+        try {
+            handler.setEncoding(handlerEncoding);
+        } catch (SecurityException | UnsupportedEncodingException e) {
+            throw new IllegalStateException("Error setting handler encoding="
+                    + handlerEncoding, e);
+        }
+
+        final Formatter socketHandlerFormatter = new XMLFormatter();
+        handler.setFormatter(socketHandlerFormatter);
+
+        final Logger rootLogger = Logger.getLogger("");
+        rootLogger.addHandler(handler);
     }
 
     public String getLoggingPropertiesFileToUse() {
--- RobotCode/test/org/usfirst/frc/team2175/robot/config/LoggingConfigurationTest.java
+++ RobotCode/test/org/usfirst/frc/team2175/robot/config/LoggingConfigurationTest.java
@@ -4,25 +4,40 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.Test;
-import org.usfirst.frc.team2175.robot.config.LoggingConfiguration;
 
 public class LoggingConfigurationTest {
     final LoggingConfiguration sut = new LoggingConfiguration();
-
-    final Logger log = Logger.getLogger(getClass().getName());
 
     /**
      * Verifies can find and read the logging config properties file and
      * displays example logging output.
      */
     @Test
-    public void testInitializeLogging() {
+    public void testInitializeFileLog() {
         sut.setLoggingPropertiesFileToUse(LoggingConfiguration.LOGGING_PROPERTIES_FILE_ACTUAL_LOCATION);
-        sut.initializeLogging();
+        sut.initializeFileLog();
 
+        Logger log = Logger.getLogger(getClass().getName());
         log.info("log a test msg");
 
         Exception e = new IllegalArgumentException("the exception msg");
         log.log(Level.SEVERE, "Exception msg", e);
+    }
+
+    @Test
+    public void testInitializeSocketLog() {
+        sut.setLoggingPropertiesFileToUse(LoggingConfiguration.LOGGING_PROPERTIES_FILE_ACTUAL_LOCATION);
+        sut.initializeSocketLog();
+        Logger log = Logger.getLogger(getClass().getName());
+        log.info("log a test msg");
+    }
+
+    @Test
+    public void testInitializeLogging() {
+        sut.setLoggingPropertiesFileToUse(LoggingConfiguration.LOGGING_PROPERTIES_FILE_ACTUAL_LOCATION);
+        sut.initializeLogging();
+
+        Logger log = Logger.getLogger(getClass().getName());
+        log.info("log a test msg");
     }
 }
