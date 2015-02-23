e45e5e5e2bc241942999edad1ad78690d76bf8f8
Bewizardment <ndgodfr@gmail.com>
2015-02-23 10:13:58 -0600

    changed some auto stuff

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/AutonMinus1Test.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/AutonMinus1Test.java
@@ -1,13 +1,25 @@
 package org.usfirst.frc.team2175.robot.commands.auto;
 
+import org.usfirst.frc.team2175.robot.Robot;
+import org.usfirst.frc.team2175.robot.commands.ZeroToteElevator;
+import org.usfirst.frc.team2175.robot.commands.single.MoveToteElevatorToPosition;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
  */
 public class AutonMinus1Test extends CommandGroup {
-    
-    public  AutonMinus1Test() {
-        
+
+    public AutonMinus1Test() {
+
+        addSequential(new ZeroToteElevator());
+        addSequential(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.stack));
+        addSequential(new WaitCommand(1000));
+        addSequential(new MoveToteElevatorToPosition(
+                Robot.properties.toteConfig.driving));
+
     }
 }
