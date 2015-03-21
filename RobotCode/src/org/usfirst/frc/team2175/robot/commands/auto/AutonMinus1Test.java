8bf7c40f9b4eb5a08678b1d58cb62084541e7998
Leonard <info@fightingcalculators.org>
2015-03-21 18:29:35 -0500

    Lots of new auto test stuff to uncomment and use.

--- RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/AutonMinus1Test.java
+++ RobotCode/src/org/usfirst/frc/team2175/robot/commands/auto/AutonMinus1Test.java
@@ -2,7 +2,9 @@ package org.usfirst.frc.team2175.robot.commands.auto;
 
 import org.usfirst.frc.team2175.robot.Robot;
 import org.usfirst.frc.team2175.robot.commands.ZeroToteElevator;
+import org.usfirst.frc.team2175.robot.commands.single.DriveInches;
 import org.usfirst.frc.team2175.robot.commands.single.MoveToteElevatorToPosition;
+import org.usfirst.frc.team2175.robot.commands.single.TurnDegrees;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -13,13 +15,56 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class AutonMinus1Test extends CommandGroup {
 
     public AutonMinus1Test() {
-
-        addSequential(new ZeroToteElevator());
-        addSequential(new MoveToteElevatorToPosition(
-                Robot.properties.toteConfig.stack));
-        addSequential(new WaitCommand(1000));
-        addSequential(new MoveToteElevatorToPosition(
-                Robot.properties.toteConfig.driving));
-
+    	
+    	// DRIVE A SQUARE
+//        addSequential(new DriveInches(36));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new TurnDegrees(90, true));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new DriveInches(36));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new TurnDegrees(90, true));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new DriveInches(36));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new TurnDegrees(90, true));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new DriveInches(36));
+//        addSequential(new WaitCommand(1));
+//        addSequential(new TurnDegrees(90, true));
+    	
+    	// DRIVE FORWARD, TURN 180, DRIVE BACK, TURN 180
+//    	addSequential(new DriveInches(72));
+//    	addSequential(new WaitCommand(1));
+//    	addSequential(new TurnDegrees(180));
+//    	addSequential(new WaitCommand(1));
+//    	addSequential(new DriveInches(72));
+//    	addSequential(new WaitCommand(1));
+//    	addSequential(new TurnDegrees(0));
+//    	addSequential(new WaitCommand(1));
+    	
+    	addSequential(new TurnDegrees(90, false));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new TurnDegrees(180, false));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new TurnDegrees(360, false));
+    	
+    	// TEST ROBONAUTS AUTO
+//    	addSequential(new TurnDegrees(-30));
+//    	addSequential(new DriveInches(8));
+//    	addSequential(new DriveInches(-8));
+//		addSequential(new TurnDegrees(0));
+//		addSequential(new DriveInches(85));
+//		addSequential(new TurnDegrees(-30));
+//		addSequential(new DriveInches(8));
+//    	addSequential(new DriveInches(-8));
+//		addSequential(new TurnDegrees(0));
+//		addSequential(new DriveInches(85));
+//		addSequential(new TurnDegrees(-30));
+//		addSequential(new DriveInches(8));
+//    	addSequential(new DriveInches(-8));
+//		addSequential(new TurnDegrees(45));
+//		addSequential(new DriveInches(102));
+//		addSequential(new DriveInches(-60));
     }
 }
